0c9245615d01ffbf3071e171acc6db18d3196d74
RavinduKavinda <dlravindukavinda@gmail.com>
2024-06-17 18:59:55 +0530

    pop up google sign in setup

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from "react";
+import { auth, provider } from "../../config/firebase-config"
+import { signInWithPopup } from "firebase/auth";
 
 export const Auth = () => {
+
+  //google sign in
+  const signInWithGoogle = async () => {
+    const results = await signInWithPopup(auth, provider);
+    console.log(results);
+  };
+
   return (
-    <div>
-        <p className="">Sign In With Google to Continue</p>
-        <button className="">Sign In with Google</button>
+    <div className="login-page">
+      <p className="">Sign In With Google to Continue</p>
+
+      <button className="login-with-google-btn" onClick={signInWithGoogle}>
+        Sign In with Google
+      </button>
     </div>
-  )
-}
+  );
+};
