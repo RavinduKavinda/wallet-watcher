32ee44960595d7438517420e5469f99558290d9c
RavinduKavinda <dlravindukavinda@gmail.com>
2024-06-17 17:53:06 +0530

    firebase config add

---
+++ src/config/firebase-config.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_fcITmZ5vMSLdUufZ65aou5oLAF1mXa0",
+  authDomain: "wallet-watcher-8d92f.firebaseapp.com",
+  projectId: "wallet-watcher-8d92f",
+  storageBucket: "wallet-watcher-8d92f.appspot.com",
+  messagingSenderId: "592652352815",
+  appId: "1:592652352815:web:05b43874b3b9e43f6e75da",
+  measurementId: "G-QQPHHCS9B5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//firebase login
+//firebase init
+//firebase deploy
