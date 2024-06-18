bd144b3c0fc3222b0cc9e3e1a610abf71d502353
RavinduKavinda <dlravindukavinda@gmail.com>
2024-06-18 17:09:26 +0530

    wallet-watcher page design

--- src/pages/expence/index.jsx
+++ src/pages/expence/index.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 export const Expense = () => {
   return (
-    <div>
-        Hello World - Expense
+    <>
+    {/* My Wallet */}
+    <div className="wallet-watcher">
+      <div className="container">
+        <h1>Wallet Watcher</h1>
+
+        {/* balance */}
+        <div className="balance">
+          <h3>My Balance</h3>
+          <h2>LKR 0.00</h2>
+        </div>
+
+        <div className="summary">
+          {/* Income */}
+          <div className="income">
+            <h4>Income</h4>
+            <p>LKR 0.00</p>
+          </div>
+          {/* Expenses */}
+          <div className="expenses">
+            <h4>Expenses</h4>
+            <p>LKR 0.00</p>
+          </div>
+        </div>
+
+        {/* add transactions */}
+        <form action="" className="add-transaction">
+          <input type="text" placeholder="Description" required />
+          <input type="number" placeholder="Amount" required />
+          <label htmlFor="expense">Expense</label>
+          <input type="radio" id="expense" value="expense" />
+          <label htmlFor="income">Income</label>
+          <input type="radio" id="income" value="income" />
+
+          <button type="submit">Add Transaction</button>
+        </form>
+      </div>
     </div>
-  )
-}
+
+    {/* Transaction History */}
+    <div className="transactions">
+      <h3>Transactions</h3>
+    </div>
+    
+    </>
+  );
+};
