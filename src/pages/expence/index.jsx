229a561fc77bf4e333574da91bc8a59c703b271c
RavinduKavinda <dlravindukavinda@gmail.com>
2024-06-18 17:54:38 +0530

    get data from form and store in firebasestore

--- src/pages/expence/index.jsx
+++ src/pages/expence/index.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAddTransaction } from "../../hooks/useAddTransaction"
 
 export const Expense = () => {
 
   const { addTransaction } = useAddTransaction();
 
+  /* get data from form */
+  const [description, setDescription] = useState("");
+  const [transactionAmount, setTransactionAmount] = useState(0);
+  const [transactionType, setTransactionType] = useState("expense");
+
   const onSubmit = (e) => {
     e.preventDefault()
     addTransaction({
-      description: "adasd",
-      transactionAmount: 23,
-      transactionType: "expense"
-    })
+      description,
+      transactionAmount,
+      transactionType,
+    });
   };
 
   return (
@@ -42,13 +47,34 @@ export const Expense = () => {
 
         {/* add transactions */}
         <form action="" className="add-transaction" onSubmit={onSubmit}>
-          <input type="text" placeholder="Description" required />
-          <input type="number" placeholder="Amount" required />
+          <input 
+            type="text" 
+            placeholder="Description" 
+            required
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <input 
+            type="number" 
+            placeholder="Amount" 
+            required
+            onChange={(e) => setTransactionAmount(e.target.value)}
+          />
           <label htmlFor="expense">Expense</label>
-          <input type="radio" id="expense" value="expense" />
+          <input 
+            type="radio" 
+            id="expense" 
+            value="expense" 
+            checked = { transactionType === "expense"}
+            onChange={(e) => setTransactionType(e.target.value)}
+          />
           <label htmlFor="income">Income</label>
-          <input type="radio" id="income" value="income" />
-
+          <input 
+            type="radio" 
+            id="income" 
+            value="income"
+            checked = { transactionType === "income"}
+            onChange={(e) => setTransactionType(e.target.value)}
+          />
           <button type="submit">Add Transaction</button>
         </form>
       </div>
