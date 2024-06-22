29944db51eb080d3c49432790e645c9e302fd60a
RavinduKavinda <dlravindukavinda@gmail.com>
2024-06-22 18:43:07 +0530

    install tailwind css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Auth } from './pages/auth/index';
 import { Expense } from './pages/expence/index';
+import './index.css';
 
 function App() {
   return (
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { auth, provider } from "../../config/firebase-config"
+import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
 export const Auth = () => {
-
   const navigate = useNavigate();
 
   //google sign in
@@ -18,18 +17,21 @@ export const Auth = () => {
       name: results.user.displayName,
       profilePhoto: results.user.photoURL,
       isAuth: true,
-    }
+    };
     localStorage.setItem("auth", JSON.stringify(authInfo));
     navigate("/wallet-watcher"); //when after sign in go to "/wallet-watcher"
   };
 
   return (
-    <div className="login-page">
-      <p className="">Sign In With Google to Continue</p>
-
-      <button className="login-with-google-btn" onClick={signInWithGoogle}>
-        Sign In with Google
-      </button>
+    <div className="container">
+      <div className="login-page">
+        <h1 className="">Welcome To Wallet Watcher</h1>
+        <p className="">Sign In With Google to Continue</p>
+        
+        <button className="login-with-google-btn" onClick={signInWithGoogle}>
+          Sign In with Google
+        </button>
+      </div>
     </div>
   );
 };
