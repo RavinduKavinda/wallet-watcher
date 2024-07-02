1263105bc4a5666785ebaa61a1d743cabdd5ae03
RavinduKavinda <dlravindukavinda@gmail.com>
2024-07-03 02:58:02 +0530

    animation border added

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
 
 export const Auth = () => {
   const navigate = useNavigate();
@@ -23,14 +24,27 @@ export const Auth = () => {
   };
 
   return (
-    <div className="container">
-      <div className="login-page">
-        <h1 className="">Welcome To Wallet Watcher</h1>
-        <p className="">Sign In With Google to Continue</p>
-        
-        <button className="login-with-google-btn" onClick={signInWithGoogle}>
-          Sign In with Google
-        </button>
+    <div
+      className="flex items-center justify-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: "url('assest/background.png')" }}
+    >
+      <div className="card-wrapper">
+        <div className="card-content ">
+          <h1 className="dm-sans-font text-[32px] font-[700] text-center leading-tight pb-6">
+            Welcome To
+            <br />
+            <span className="text-cyan-500	text-[40px]">Wallet Watcher !</span>
+          </h1>
+          <div className="flex justify-center ">
+            <button
+              className="flex items-center bg-blue-500 border border-blue-500 text-white rounded-lg text-[16px] font-semibold gap-2 pr-3 hover:bg-blue-700 transition"
+              onClick={signInWithGoogle}
+            >
+              <FcGoogle className=" bg-white text-[40px] rounded-l-lg p-2" />{" "}
+              Sign In With Google
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 7s linear infinite'
+      }
+    },
   },
   plugins: [],
 }
